f16406fa2edc51a29d96ba1212588189c36f50f5
Mohit Soni <sonimohit481@gmail.com>
2024-01-21 20:32:35 +0530

    Basic route made

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <NavLink
-                  to={"/home"}
+                  to={"/"}
                   className={({ isActive }) =>
                     `block py-2 pr-4 pl-3 ${
                       isActive ? "text-orange-700" : "text-gray-700"
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout.tsx";
+import Home from "./components/home/Home.tsx";
+import About from "./components/about/About.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
